813d8817064953c6c76878262771e72d9b01b140
nneka <okoronnekajuliet@gmail.com>
2022-09-08 22:33:47 -0700

    my sixth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main -Serves as the entry point for the program
+ * main- Serves as the entry point for the program
  *
- * Return:By default return zero if no errors otherwise return non-zero value
+ * Return: By default return zero if no errors otherwise return non-zero value
  */
 int main(void)
 {
