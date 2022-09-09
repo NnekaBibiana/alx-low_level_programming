062f30d565632fabf609f82425027860d6174363
nneka <okoronnekajuliet@gmail.com>
2022-09-08 20:41:33 -0700

    my sixth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main-S:By default returns zero if no errors otherwise return non-zero valueaa
-lue.
- * Return -By default returns zero if no errors otherwise return non-zero value
+ * main -Serves as the entry point for the program
+ *
+ * Return:By default return zero if no errors otherwise return non-zero value
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\" Programming is like building a multilingual puzzle');
 	return (0);
 }
