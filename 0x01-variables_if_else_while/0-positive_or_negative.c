f08f4fed89443dd30e01069862f6efaac1af1bd3
nneka <okoronnekajuliet@gmail.com>
2022-09-10 09:55:32 -0700

    variable commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,25 +4,20 @@
 /**
  * main- determine if a number is positive or negative
  * description- program will assign a random number to the variable each time
+ * return: if true print zero if not return non-zero
  *
- * return: if true print zero if not return non zero
  */
 int main(void)
 {
 		int n;
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
 
-
-				
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-			       if (n==0)
+			       if (n == 0)
 				       printf("%d is zero\n", n);
-			       else if (n<0)
+			       else if (n < 0)
 				       printf("%d is negative\n", n);
 			       else 
-				       printf("%d is positive\n",n);
+				       printf("%d is positive\n", n);
 		       	       return (0);
-
 }
