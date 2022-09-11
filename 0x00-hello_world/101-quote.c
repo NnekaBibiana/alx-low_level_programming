4a0f672ce1bc67610f42ae1b8051744e9610c058
nneka <okoronnekajuliet@gmail.com>
2022-09-11 13:06:21 -0700

    my sixth commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * 
+ * Return: always 0
+*/
+int main(void)
+{
+	fprintf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
