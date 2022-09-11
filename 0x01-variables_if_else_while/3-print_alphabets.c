c9937c91ef08a81dc83f91f696813504aad3d518
nneka <okoronnekajuliet@gmail.com>
2022-09-10 17:21:37 -0700

    variable commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - start a program that prints all cases
+ *
+ * Return: always 0 (correct)
+ *
+ */
+int main(void)
+{
+	char r;
+	char s;
+
+	for (r == 'a'; r <= 'z'; r++)
+	{
+		putchar (r);
+	}
+	for (s == 'A'; s <= 'Z'; s++)
+	{
+		putchar (s);
+	}
+	putchar ('\n');
+	return (0);
+}
