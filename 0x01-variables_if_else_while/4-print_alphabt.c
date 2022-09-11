920676a23b868e37aef64ebf4a5b5a3d06318bf5
nneka <okoronnekajuliet@gmail.com>
2022-09-11 06:40:25 -0700

    variable commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,12 +10,13 @@
 int main(void)
 {
 	char i;
-		for (i = 'a'; i <= 'z'; i++)
-		{
-		if (i != 'q' && i != 'e')
-			putchar(i );
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+			if (i != 'q' && i != 'e')
+				putchar(i );
 		}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
 
