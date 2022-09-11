7bc4eba62f5c7c934db56903a7597243767972e0
nneka <okoronnekajuliet@gmail.com>
2022-09-11 06:08:14 -0700

    variable commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - start program that prints letters without q and a
+ *
+ * Return: anser zero
+ *
+ */
+
+int main(void)
+{
+	char p;
+		for (p = 'a'; p <= 'z'; p++)
+		{
+		if (p <= 'q' && 'e';)
+			putchar(p);
+		}
+	putcher("\n");
+	return (0);
+}
+
