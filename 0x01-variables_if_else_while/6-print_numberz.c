50c174c2ac1f8ea425959537217ebb2423608507
nneka <okoronnekajuliet@gmail.com>
2022-09-12 15:15:31 -0700

    variable commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - starting program 
+ * main - starting program
  *
  * Return: always zero
  *
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
