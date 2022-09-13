74ecd01074bd70d9b4861943f8197c32dbc29a6d
nneka <okoronnekajuliet@gmail.com>
2022-09-12 20:11:46 -0700

    variable commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.>
+/**
+ * main - entry point
+ *
+ * Return: always zero
+ */
+int main (void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
