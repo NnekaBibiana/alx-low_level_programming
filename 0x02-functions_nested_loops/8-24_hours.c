8df8c5a2d47cb505fc75473cb21696119362c8de
nneka <okoronnekajuliet@gmail.com>
2022-09-14 18:07:34 -0700

    putchar

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,14 +2,16 @@
 
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- *
- * Return:  from 00:00 to 23:59
+ * 
+ * starting from 00:00 to 23:59
  */
 
 void jack_bauer(void)
 
 {
+
 	int i, j;
+
 	i = 0;
 
 	while (i < 24)
