d220862af0bc293d1a8a4bb0edaef105f0f2e4fa
nneka <okoronnekajuliet@gmail.com>
2022-09-16 18:22:00 -0700

    loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,26 +7,28 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-	int b;
-
-	if (n  > 0)
+	if (n <= 0)
 	{
+		_putchar('\n');
+	}
+	else
+	{
+
+	int i;
+	int j;
+
 		for (i = 0; i < n; i++)
 		{
-			for (b = 0; b < 1; b++)
+			for (j = 0; j < n; j++)
 			{
-				_putchar (' ');
+				if (j == i)
+					_putchar('\\');
+				else if (j < 1)
+					_putchar (' ');
 			}
-			_putchar (92);
 			_putchar ('\n');
 
 		}
 
-	}
-
-	else
-	{
-		_putchar ('\n');
 	}
 }
