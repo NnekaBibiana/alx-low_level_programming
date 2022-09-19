658f4d618145ad3fe066181473812befc09944aa
nneka <okoronnekajuliet@gmail.com>
2022-09-19 07:26:19 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer and reset the value it points
+ * @n: a useless integer
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of the string 
+ * @s : string
+ * Return: length
+ */
+
+int - strlen ( char *s)
+{
+	char *str; 
+	int len ;
