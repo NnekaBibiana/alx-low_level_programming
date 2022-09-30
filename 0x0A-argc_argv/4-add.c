c22dddaa013adaebd6cdc66d914355afaf528951
nneka <okoronnekajuliet@gmail.com>
2022-09-30 15:01:25 -0700

    argc

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - adds positive number
+ * @argv: number of arguments
+ * @argc: array of arguments
+ * Return: 1 if not intger
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, num, i, j, k;
+
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		for (j + 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
