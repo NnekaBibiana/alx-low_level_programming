ce8894cc97564fe4df6ba63b51367fb5358acd6a
nneka <okoronnekajuliet@gmail.com>
2022-09-30 14:09:06 -0700

    argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	{
 		int result;
 
-		result = _atoi(argv[1]) * _atoi(argv[2]);
+		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d|n", result);
 	return (0);
 	}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,3 +1,3 @@
 int _putchar(char c);
 
-int _atoi(char *s);
+int atoi(char *s);
