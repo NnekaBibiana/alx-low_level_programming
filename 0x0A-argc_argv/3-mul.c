4e34aa223bd360bb6328fed169750135551dbb59
nneka <okoronnekajuliet@gmail.com>
2022-09-30 12:25:56 -0700

    argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,17 @@
 
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int Result, num1, num2;
 
-	if (argc < 3 || argc > 3)
-	{
-		printf("Error\n");
-
-	return (1);
-	}
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
-	result = num1 * num2;
-	printf("%d\n", result);
+       
+        Result = _atoi(argv[1]) * _atoi(argv[2]);
+	{
+
+	printf("%d\n", Result);
+	}
+
 	return (0);
 }
 
