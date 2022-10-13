d2ea5c9d9f00850c6eb8538d54dd14864300e9a7
nneka <okoronnekajuliet@gmail.com>
2022-10-13 12:12:34 -0700

    functionpointers

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - function pointer that selects thecorrect function to perform
+ * the operation asked by the iser
+ * @s: the operation given by the user
+ *
+ * Return: pointer to the function that corresponds
+ * to the operator given to a parameter
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_sub},
+		{"/", op_mul},
+		{"%", op_mod},
+		{NULL, NULL}
+
+	};
+
+	int i;
+
+	i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculate the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calculate the difference of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculate the multiple of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: multiplication of a and b
+ */
+int  op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: divides two numberz
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates the remainder of the division
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
