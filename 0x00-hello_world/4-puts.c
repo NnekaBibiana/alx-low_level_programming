8b86b7d8be56e02eae7781ead8d09c88e39e0406
NnekaBibiana <okoronnekajuliet@gmail.com>
2023-07-13 19:29:27 +0100

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main()
+{
+	char s="\"Programming is like building a multilingual puzzle";
+	puts(s);
+	return 0;
+}
