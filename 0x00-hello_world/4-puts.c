2a6875cb601cdeb0a289c8b4aa581a1f874740e8
NnekaBibiana <okoronnekajuliet.gmail.com>
2023-07-13 11:41:43 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,10 @@
  *
  * Return: Always 0
  */
-int main()
+int main(void)
 {
-	char s[]="\"Programming is like building a multilingual puzzle";
+	char s[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(s);
-	return 0;
+	return (0);
 }
