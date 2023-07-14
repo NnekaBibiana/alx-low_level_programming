299eca622a9b7d9da2e56c037412e75370840034
NnekaBibiana <okoronnekajuliet.gmail.com>
2023-07-14 13:38:37 -0700

    variable if else

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always success
+ */
+int main(void)
+{
+        char lower;
+
+        for (lower = 'a'; lower <= 'z'; lower++)
+        {
+		if (lower == 'q' || lower == 'e')
+		{
+			continue;
+		}
+                putchar(lower);
+
+        }
+                putchar('\n');
+                return (0);
+}
