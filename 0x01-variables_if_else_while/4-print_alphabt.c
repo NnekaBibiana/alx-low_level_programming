b5711400f6a93de6200928cd3e374ac95279c4d6
NnekaBibiana <okoronnekajuliet.gmail.com>
2023-07-14 13:43:43 -0700

    variable if else

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,17 +6,16 @@
  */
 int main(void)
 {
-        char lower;
+	char lower;
 
-        for (lower = 'a'; lower <= 'z'; lower++)
-        {
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
 		if (lower == 'q' || lower == 'e')
 		{
 			continue;
 		}
-                putchar(lower);
-
-        }
-                putchar('\n');
-                return (0);
+		putchar(lower);
+	}
+	putchar('\n');
+	return (0);
 }
