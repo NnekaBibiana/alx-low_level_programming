98b2fdd4e8c3eb24e5de130f816e4285b745510c
NnekaBibiana <okoronnekajuliet.gmail.com>
2023-07-19 17:20:11 -0700

    functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,12 @@
 int print_last_digit(int c)
 {
 	int j = c % 10;
-	
+
 	if (j < 0)
 	{
 		j = j * -1;
 	}
 	_putchar (j + '0');
 
-	return(j);
+	return (j);
 }
